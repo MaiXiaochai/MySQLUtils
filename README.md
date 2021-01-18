16a7ad3c469531700341f1f85bd6bde180717f4e
MaiXiaochai <maixiaochai@qq.com>
2021-01-18 19:47:25 +0800

    +)func:MySQLUtils.has_table;简短的使用说明

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MySQLUtils
 MySQL功能封装。
+
++ 使用说明
+    + `pip install pymysql`
+    + 在`demo.py`中有使用案例
--- mysql_utils.py
+++ mysql_utils.py
@@ -16,8 +16,6 @@ from pymysql.cursors import DictCursor
 class MySQLUtils:
     """MySQL 基本功能封装 """
 
-    __exist_sql = " select * from {0} where {1} = %({1})s"
-
     def __init__(self,
                  host: str,
                  port: int,
@@ -26,13 +24,13 @@ class MySQLUtils:
                  db: str):
 
         self.conn = connect(
-            host=host,
-            port=port,
-            user=user,
-            passwd=passwd,
-            db=db,
-            charset='utf8',  # utf8mb4 是utf8的超集
-            cursorclass=DictCursor)  # 返回类字典类型游标
+                host=host,
+                port=port,
+                user=user,
+                passwd=passwd,
+                db=db,
+                charset='utf8',  # utf8mb4 是utf8的超集
+                cursorclass=DictCursor)  # 返回类字典类型游标
         self.cur = self.conn.cursor()
 
     def execute(self, sql, args=None):
@@ -54,15 +52,23 @@ class MySQLUtils:
 
         return result
 
+    def has_table(self, table_name):
+        """
+            该用户下是否存在表table_name
+        """
+        sql = "SELECT count(*) FROM information_schema.TABLES WHERE table_name =%(table_name)s"
+        arg = {'table_name': table_name}
+        return self.fetchone(sql, arg)[table_name] == 1
+
     def exist_data_by_kw(self, table_name: str, data: dict):
         """表table_name中是否存在where key = value的数据"""
         k, = data
-        sql = self.__exist_sql.format(table_name, k)
-        f_data = self.fetchone(sql, data)
 
-        result = False
-        if f_data:
-            result = True
+        exist_sql = "select * from {0} where {1} = %({1})s"
+        sql = exist_sql.format(table_name, k)
+
+        f_data = self.fetchone(sql, data)
+        result = True if f_data else False
 
         return result
 
